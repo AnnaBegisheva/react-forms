cb93065dd5761ea2c6f72b224a1105690f272f94
AnnaBegisheva <annabegisheva@gmail.com>
2024-11-14 12:51:04 +0700

    init project, set up vite and ts configs

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import { Button } from 'react-forms';
+
+function App() {
+  return (
+    <>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
